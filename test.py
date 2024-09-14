855ef17f7dcb0ec69256c6c55b4a6a264ca1448a
Justin Diamond <54919223+Justin-Diamond@users.noreply.github.com>
2024-09-14 17:38:54 -0400

    Update test.py

--- test.py
+++ test.py
@@ -2,23 +2,23 @@ import os
 import json
 import random
 import requests
-import tweepy
+from requests_oauthlib import OAuth1Session
 
 # URL of the consolidated list
 CONSOLIDATED_LIST_URL = "https://data.trade.gov/downloadable_consolidated_screening_list/v1/consolidated.json"
 
 # Twitter API credentials
-TWITTER_API_KEY = os.environ.get("TWITTER_API_KEY")
-TWITTER_API_SECRET = os.environ.get("TWITTER_API_SECRET")
-TWITTER_ACCESS_TOKEN = os.environ.get("TWITTER_ACCESS_TOKEN")
-TWITTER_ACCESS_TOKEN_SECRET = os.environ.get("TWITTER_ACCESS_TOKEN_SECRET")
+CONSUMER_KEY = os.environ.get("CONSUMER_KEY")
+CONSUMER_SECRET = os.environ.get("CONSUMER_SECRET")
+ACCESS_TOKEN = os.environ.get("ACCESS_TOKEN")
+ACCESS_TOKEN_SECRET = os.environ.get("ACCESS_TOKEN_SECRET")
 
 def check_credentials():
     credentials = {
-        "TWITTER_API_KEY": TWITTER_API_KEY,
-        "TWITTER_API_SECRET": TWITTER_API_SECRET,
-        "TWITTER_ACCESS_TOKEN": TWITTER_ACCESS_TOKEN,
-        "TWITTER_ACCESS_TOKEN_SECRET": TWITTER_ACCESS_TOKEN_SECRET
+        "CONSUMER_KEY": CONSUMER_KEY,
+        "CONSUMER_SECRET": CONSUMER_SECRET,
+        "ACCESS_TOKEN": ACCESS_TOKEN,
+        "ACCESS_TOKEN_SECRET": ACCESS_TOKEN_SECRET
     }
     
     for key, value in credentials.items():
@@ -34,15 +34,31 @@ def get_random_name():
     return random.choice(names)
 
 def send_tweet(message):
-    auth = tweepy.OAuthHandler(TWITTER_API_KEY, TWITTER_API_SECRET)
-    auth.set_access_token(TWITTER_ACCESS_TOKEN, TWITTER_ACCESS_TOKEN_SECRET)
-    api = tweepy.API(auth)
-    
-    try:
-        api.update_status(message)
-        print(f"Tweet sent successfully: {message}")
-    except tweepy.errors.TweepError as e:
-        print(f"Error sending tweet: {str(e)}")
+    payload = {"text": message}
+
+    # Make the request
+    oauth = OAuth1Session(
+        CONSUMER_KEY,
+        client_secret=CONSUMER_SECRET,
+        resource_owner_key=ACCESS_TOKEN,
+        resource_owner_secret=ACCESS_TOKEN_SECRET,
+    )
+
+    # Making the request
+    response = oauth.post(
+        "https://api.twitter.com/2/tweets",
+        json=payload,
+    )
+
+    if response.status_code != 201:
+        raise Exception(
+            f"Request returned an error: {response.status_code} {response.text}"
+        )
+
+    print(f"Tweet sent successfully: {message}")
+    print(f"Response code: {response.status_code}")
+    json_response = response.json()
+    print(json.dumps(json_response, indent=4, sort_keys=True))
 
 def main():
     print("Checking Twitter API credentials:")
